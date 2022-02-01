5145299ab779316882aa119fddb6e2662aa5629e
tauron19 <tauron19@gmail.com>
2022-02-01 02:08:23 -0500

    search

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { robots } from './robots';
 
 import 'tachyons';
 
 
 ReactDOM.render(
   <React.StrictMode>    
-          <CardList robots ={robots}/>
+          <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
