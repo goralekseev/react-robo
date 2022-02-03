fa20645fd6c608d85f674a078253c6729f364208
tauron19 <tauron19@gmail.com>
2022-02-03 03:22:51 -0500

    font

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css';
 
 
 class App extends Component{
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
@@ -26,7 +27,7 @@ class App extends Component{
 
         return(
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f2'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <CardList robots ={filteredRobots}/>
             </div>
